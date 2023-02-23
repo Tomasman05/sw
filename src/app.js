268d8760ad3ef444a3863a7c84063ca0b67f3504
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-02-23 09:59:16 +0100

    finished

---
+++ src/app.js
@@ -0,0 +1,9 @@
+var cim = document.querySelector("#cim")
+var div = document.createElement("div")
+cim.appendChild(div)
+div.textContent= "alma" 
+var i=0
+while (i<5) {
+    i++
+    console.log(i)
+}
